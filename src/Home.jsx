ba5cde810d2d8607f7aa6466a5962067d263cb76
Rohithbb19 <rohithbb19@gmail.com>
2022-10-05 21:23:55 +0530

    Add Projects

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,12 +24,12 @@ function Home(){
                 <p className="text-xl font-bold">Projects</p>
                 <div className="flex flex-col tablet:gap-6 tablet:flex-row">
                 <div className="p-2 shadow-2xl rounded mt-5 w-80 flex flex-col justify-center duration-150 hover:bg-purple-500 hover:text-white">
-                    <img src="public\Cal.PNG"/>
-                    <p className="font-medium text-center mt-1">Calculator</p>
-                    <p className="mt-2 text-center">it's a calculator web application build using HTML, CSS and Javascript</p>
+                    <img src="public\ACME.PNG"/>
+                    <p className="font-medium text-center mt-1">ACME Dashbord</p>
+                    <p className="mt-2 text-center">it's a Dashbord web application build using React.js and required Libraries</p>
                     <div className="flex justify-around mt-4 mb-2">
-          <a href="https://github.com/Rohithbb19/calculator" target="_blank" rel="noopener noreferrer" className="p-1 px-4 rounded bg-purple-300 duration-150 hover:scale-110">Code</a>
-          <a href="https://rohithbb19.github.io/calculator/" target="_blank" rel="noopener noreferrer" className="p-1 px-4 rounded bg-purple-300 duration-150 hover:scale-110">Demo</a>
+          <a href="https://github.com/Rohithbb19/mitsogo-round1" target="_blank" rel="noopener noreferrer" className="p-1 px-4 rounded bg-purple-300 duration-150 hover:scale-110">Code</a>
+          <a href="https://mitsogo-round1.vercel.app/" target="_blank" rel="noopener noreferrer" className="p-1 px-4 rounded bg-purple-300 duration-150 hover:scale-110">Demo</a>
           </div>
                 </div>
                 <div className="p-2 shadow-2xl rounded mt-5 w-80 flex flex-col justify-center duration-150 hover:bg-purple-500 hover:text-white">
